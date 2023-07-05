3822aa4aea81d24b872e015c65fdfc98065bbde8
Kamran <kamran.07@icloud>
2023-07-05 13:12:09 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,14 +3,6 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
-/**
- *  Создание таблицы User(ов)
- *  Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления должен быть вывод в
- *  консоль ( User с именем – name добавлен в базу данных )
- *  Получение всех User из базы и вывод в консоль ( должен быть переопределен toString в классе User)
- *  Очистка таблицы User(ов)
- *  Удаление таблицы
- */
 public class Main {
     private final static UserService userService = new UserServiceImpl();
 
