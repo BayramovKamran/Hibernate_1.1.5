28706f6682bd708397ff0db99d7ddc170ad40341
Kamran <kamran.07@icloud>
2023-07-19 13:04:13 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,13 +1,6 @@
 package jm.task.core.jdbc.dao;
 
-import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
+import jm.task.core.jdbc.model.User;;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
